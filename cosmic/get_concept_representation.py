a737c90299604fb85ad047988b55c40dfa160488
Mysteriousplayer <wangskkk@163.com>
2020-11-21 17:39:36 +0800

    Add files via upload

--- cosmic/get_concept_representation.py
+++ cosmic/get_concept_representation.py
@@ -15,4 +15,4 @@ data=standardization(w)
 #for p in data:
     #print(p)
 print(data.shape)
-np.save('D:\\image_tag\\tag_dataset\\embed\\embedding_vec_100_new_without_text_v2.npy',data)
+np.save('D:\\image_tag\\tag_dataset\\embed\\concept_vec_100_new_without_text_v2.npy',data)
